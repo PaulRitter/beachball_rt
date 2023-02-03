4c604f86b23c7cdbbf540276b2f296f87181ff44
Exp <17885980+exp111@users.noreply.github.com>
2023-02-03 18:53:00 +0100

    hline

---
+++ Content.Client/UserInterface/Controls/HLine.cs
@@ -0,0 +1,50 @@
+﻿using Robust.Client.Graphics;
+using Robust.Client.UserInterface.Controls;
+using Robust.Shared.Maths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Content.Client.UserInterface.Controls
+{
+    public sealed class HLine : Container
+    {
+        public Color? Color
+        {
+            get
+            {
+                if (_line.PanelOverride is StyleBoxFlat styleBox) return styleBox.BackgroundColor;
+                return null;
+            }
+            set
+            {
+                if (_line.PanelOverride is StyleBoxFlat styleBox) styleBox.BackgroundColor = value!.Value;
+            }
+        }
+
+        public float? Thickness
+        {
+            get
+            {
+                if (_line.PanelOverride is StyleBoxFlat styleBox) return styleBox.ContentMarginTopOverride;
+                return null;
+            }
+            set
+            {
+                if (_line.PanelOverride is StyleBoxFlat styleBox) styleBox.ContentMarginTopOverride = value!.Value;
+            }
+        }
+
+        private readonly PanelContainer _line;
+
+        public HLine()
+        {
+            _line = new PanelContainer();
+            _line.PanelOverride = new StyleBoxFlat();
+            _line.PanelOverride.ContentMarginTopOverride = Thickness;
+            AddChild(_line);
+        }
+    }
+}
